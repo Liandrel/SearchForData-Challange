fd362e8d5e857d1220155f881c5b88a9358df419
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-10-08 21:09:14 +0200

    Form UI
    Created form with listbox and textbox and button to show search for data functions in given files for main goal/ bonus goal

--- SearchForDataChall/Form1.Designer.cs
+++ SearchForDataChall/Form1.Designer.cs
@@ -28,12 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SearchBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SearchResults = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ChallangeChoice = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SearchBox
+            // 
+            this.SearchBox.Location = new System.Drawing.Point(15, 70);
+            this.SearchBox.Margin = new System.Windows.Forms.Padding(6);
+            this.SearchBox.Name = "SearchBox";
+            this.SearchBox.Size = new System.Drawing.Size(299, 39);
+            this.SearchBox.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(353, 68);
+            this.button1.Margin = new System.Windows.Forms.Padding(6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 49);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // SearchResults
+            // 
+            this.SearchResults.FormattingEnabled = true;
+            this.SearchResults.ItemHeight = 32;
+            this.SearchResults.Location = new System.Drawing.Point(15, 176);
+            this.SearchResults.Margin = new System.Windows.Forms.Padding(6);
+            this.SearchResults.Name = "SearchResults";
+            this.SearchResults.Size = new System.Drawing.Size(1304, 484);
+            this.SearchResults.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 32);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 32);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Search Box";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 137);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 32);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "results";
+            // 
+            // ChallangeChoice
+            // 
+            this.ChallangeChoice.FormattingEnabled = true;
+            this.ChallangeChoice.Location = new System.Drawing.Point(811, 69);
+            this.ChallangeChoice.Name = "ChallangeChoice";
+            this.ChallangeChoice.Size = new System.Drawing.Size(428, 40);
+            this.ChallangeChoice.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(811, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 32);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Goal/bonus";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1332, 674);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ChallangeChoice);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SearchResults);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.SearchBox);
+            this.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private TextBox SearchBox;
+        private Button button1;
+        private ListBox SearchResults;
+        private Label label1;
+        private Label label2;
+        private ComboBox ChallangeChoice;
+        private Label label3;
     }
 }
--- SearchForDataChall/Form1.cs
+++ SearchForDataChall/Form1.cs
@@ -1,10 +1,68 @@
+using System.ComponentModel;
+
 namespace SearchForDataChall
 {
     public partial class Form1 : Form
     {
+        BindingList<string> searchBoxData = new();
         public Form1()
         {
             InitializeComponent();
+            SearchResults.DataSource = searchBoxData;
+            ChallangeChoice.DataSource = Enum.GetValues(typeof(challangeChoice));
+        }
+        
+        enum challangeChoice
+        {
+            MainGoal,
+            BonusChallange
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SearchForDataProcessor sfd = new();
+            switch (ChallangeChoice.SelectedItem)
+            {
+                case challangeChoice.MainGoal:
+
+                    searchBoxData.Clear();
+                    
+                    List<string> results = sfd.SearchForText(SearchBox.Text, "primary.txt");
+
+                    UpdateSearchResultsList(searchBoxData, results);
+                    SearchBox.Text = "";
+
+                    if(searchBoxData.Count == 0)
+                    {
+                        searchBoxData.Add("No results for the given string");
+                    }
+
+                    break;
+                case challangeChoice.BonusChallange:
+
+                    searchBoxData.Clear();
+
+                    List<string> phoneNumbers = sfd.SearchForPhoneNumbers("bonus.txt");
+
+                    UpdateSearchResultsList(searchBoxData, phoneNumbers);
+                    SearchBox.Text = "";
+
+                    if (searchBoxData.Count == 0)
+                    {
+                        searchBoxData.Add("No phone numbers found in text");
+                    }
+
+                    break;
+
+            }
+        }
+
+        private void UpdateSearchResultsList(BindingList<string> searchBoxData, List<string> results)
+        {
+            foreach(string result in results)
+            {
+                searchBoxData.Add(result);
+            }
         }
     }
 }
